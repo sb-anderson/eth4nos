08f3727c7eb7cc6b3c1819f472812ea2135aa574
ricegood <yeonjaekoo@gmail.com>
2019-07-22 16:55:39 +0900

    Init private chain

---
+++ build/bin/full.sh
@@ -0,0 +1,1 @@
+./geth --datadir "./db" --networkid 12345 --rpc --rpcport "8081" --rpccorsdomain "*" --port 30303 --nodiscover --rpcapi="admin,db,eth,debug,miner,net,shh,txpool,personal,web3" --allow-insecure-unlock console
---
+++ build/bin/sync.sh
@@ -0,0 +1,1 @@
+./geth --datadir "./db2" --networkid 12345 --rpc --rpcport "8082"  --rpccorsdomain "*"  --port 30304 --nodiscover --rpcapi="admin,db,eth,debug,miner,net,shh,txpool,personal,web3" --ipcdisable console
