fe68650988a6718fb46f27e27ebb133f2b58164a
jmlee <jmlee@altair.snu.ac.kr>
2019-07-31 18:15:58 +0900

    basic script for experiment

--- build/bin/expScript.js
+++ build/bin/expScript.js
@@ -22,6 +22,7 @@ for (var blockNum = 1; blockNum <= targetBlockNum; blockNum++) {
   for (var i = 0; i < txNum; i++){
     //SendTransaction()
     eth.sendTransaction({from:eth.accounts[0], to:eth.accounts[1], value: web3.toWei(0.05, "ether"), gas:21000}); // for test
+    console.log("pending transaction count:", eth.pendingTransactions.length)
   }
 
   // make block
