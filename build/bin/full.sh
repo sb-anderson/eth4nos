73a301b2f0941f83b4f2878e395ea04af8f9a40c
jmlee <jmlee@altair.snu.ac.kr>
2019-07-31 15:33:02 +0900

    script for experiment

--- build/bin/full.sh
+++ build/bin/full.sh
@@ -1,6 +1,6 @@
 if [ $1 -eq 1 ]
 then
-  ./geth --datadir "./db" --gcmode archive --networkid 12345 --rpc --rpcport "8081" --rpccorsdomain "*" --port 30303 --nodiscover --rpcapi="admin,db,eth,debug,miner,net,shh,txpool,personal,web3" --allow-insecure-unlock --preload "script.js" console
+  ./geth --datadir "./db" --gcmode archive --networkid 12345 --rpc --rpcport "8081" --rpccorsdomain "*" --port 30303 --nodiscover --rpcapi="admin,db,eth,debug,miner,net,shh,txpool,personal,web3" --allow-insecure-unlock --preload "expScript.js" console
 else
     ./geth --datadir "./db" --gcmode archive --networkid 12345 --rpc --rpcport "8081" --rpccorsdomain "*" --port 30303 --nodiscover --rpcapi="admin,db,eth,debug,miner,net,shh,txpool,personal,web3" --allow-insecure-unlock console
 fi
