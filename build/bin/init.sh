f15da2a7e7d574ccde7996832acfa691b36d1ca3
ricegood <yeonjaekoo@gmail.com>
2019-07-31 15:07:21 +0900

    Change shell script for keystore directory

--- build/bin/init.sh
+++ build/bin/init.sh
@@ -1,4 +1,3 @@
 rm -rf db*
 ./geth --datadir "./db" init genesis.json
 ./geth --datadir "./db2" init genesis.json
-cp keystore/UTC--2019-07-26T09-16-38.605947084Z--c4422d1c18e9ead8a9bb98eb0d8bb9dbdf2811d7 db/keystore/
