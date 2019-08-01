57066c77eea50eddb13abecd1a66dcdcf0d576b1
jmlee <jmlee@altair.snu.ac.kr>
2019-08-01 15:03:35 +0900

    fix experiment scripts

--- build/bin/experiment/expScriptInner.js
+++ build/bin/experiment/expScriptInner.js
@@ -1,5 +1,8 @@
 targetBlockNum = 10000;
 
+personal.unlockAccount(eth.accounts[0], "1234", 0);
+eth.sendTransaction({from:eth.coinbase, to:eth.accounts[0], value: web3.toWei(1, "ether"), gas:21000});
+
 // make blockchain
 for (var blockNum = 1; blockNum <= targetBlockNum; blockNum++) {
 
@@ -7,11 +10,22 @@ for (var blockNum = 1; blockNum <= targetBlockNum; blockNum++) {
   var txNum = 1
 
   // wait for tx
-  while(eth.pendingTransactions.length < txNum) {}
+  console.log("wait for txs")
+  var ptxCount = 0
+  while(eth.pendingTransactions.length < txNum) {
+    if (eth.pendingTransactions.length>ptxCount){
+      ptxCount = eth.pendingTransactions.length
+      console.log("pending tx count: ", ptxCount)
+    }
+    //console.log("wait for txs")
+    //console.log("pending tx count: ", eth.pendingTransactions.length)
+  }
+  console.log("tx received. start mining")
 
   // make 1 block
   miner.start()
   while(eth.blockNumber < blockNum) {}
   miner.stop()
+  console.log("successfully mined a block")
 
 }
--- build/bin/experiment/expScriptOutter.js
+++ build/bin/experiment/expScriptOutter.js
@@ -1,64 +1,76 @@
-targetBlockNum = 10000;
-accountsNum = 500;
-activeAccountsNum = 50;
-sizeCheckEpoch = 1000;
-storageFolder = "/home/jmlee/go/src/github.com/eth4nos/go-ethereum/build/bin/dataa/geth/chaindata"
-epoch = 500;
+var targetBlockNum = 10000;
+var accountsNum = 500;
+var activeAccountsNum = 50;
+var sizeCheckEpoch = 1000;
+var storageFolder = "/home/jmlee/go/src/github.com/eth4nos/go-ethereum/build/bin/dataa/geth/chaindata"
+var epoch = 500;
 
-activeAccounts = []
+var activeAccounts = []
 
 const Web3 = require("web3");
 const getSize = require('get-folder-size')
-var exec = require("child_process").exec, child;
 
 const provider = "http://localhost:8877";
 const web3 = new Web3(provider);
 
-// unlock accounts (until exit geth)
-/*for (var i = 0; i < accountsNum; i++) {
-  personal.unlockAccount(eth.accounts[i], "1234", 0);
-}*/
+console.log("start script")
 
-// send transactions
-for (var blockNum = 1; blockNum <= targetBlockNum; blockNum++) {
-
+async function foo(blockNum) {
   // # of tx in a block
   var txNum = 1
 
-
-
   // send transactions
   for (var i = 0; i < txNum; i++){
-    SendTransaction()
+    await SendTransaction()
     //eth.sendTransaction({from:eth.accounts[0], to:eth.accounts[1], value: web3.toWei(0.05, "ether"), gas:21000}); // for test
   }
+
+  // wait for mining
+  console.log("waiting for mining")
+  var mined = false
+  while(true){
+    
+    await web3.eth.getBlockNumber(function (error, result) {
+      if (error){
+        console.log("get block number error: ", error)
+      }
+
+      if(!error) {
+        console.log("block number: ", result);
+        //res.send("It is me");
+      }
+
+      if (result >= blockNum){
+        //console.log("block mined!")
+        mined = true
+      }
+  
+    })
+
+    if (mined){
+      console.log("block mined!")
+      break
+    }
+
+  }
+
+
+
 
   // check storage size
   web3.eth.getBlockNumber(function (error, result) {
     if(!error) {
-      console.log(result);
-      res.send("It is me");
+      console.log("block number: ", result);
+      //res.send("It is me");
     }
 
     if (result % sizeCheckEpoch == 0){
-      /*getSize(storageFolder, (err, size) => {
+      getSize(storageFolder, (err, size) => {
         if (err) { throw err; }
-       
+      
         console.log(size + ' bytes');
         console.log((size / 1024 / 1024).toFixed(2) + ' MB');
-      });*/
-
-      child = exec('df -h',
-      function (error, stdout, stderr) {
-          console.log('stdout: ' + stdout);
-          console.log('stderr: ' + stderr);
-          if (error !== null) {
-              console.log('exec error: ' + error);
-          }
       });
-      child();
-
-
 
     }
 
@@ -67,36 +79,52 @@ for (var blockNum = 1; blockNum <= targetBlockNum; blockNum++) {
   if (blockNum % epoch == 0){
     SetActiveAccounts()
   }
-
-
 }
+
+(async function() {
+  // send transactions
+  for (var blockNum = 2; blockNum <= targetBlockNum; blockNum++) {
+    await foo(blockNum);
+  }
+})();
+
+
+
 
 var activeIndex = 0
 
 function SendTransaction(){
+  return new Promise((resolve, reject) => {
+    console.log("start send transaction")
 
-
-  web3.eth.getAccounts().then(accounts => {
-    var address = accounts[0];
-    var password = "1234"
-    web3.eth.personal.unlockAccount(address, password, 0).then(() => {
-      web3.eth.sendTransaction({
-            from: address,
-            to: "0x0123456789012345678901234567890123456789",
-            value: 1,
-            gas: 21000000,
-            data: "0x" + toHexString(rlped),
-        }, function (err, hash) {
-            console.log("> txHash   : ", hash);
-        });
+    web3.eth.getAccounts().then(accounts => {
+      console.log("accounts length: ", accounts.length)
+      var address = accounts[0];
+      var password = "1234"
+      web3.eth.personal.unlockAccount(address, password, 0).then(() => {
+        web3.eth.sendTransaction({
+              from: address,
+              to: "0x0123456789012345678901234567890123456999",
+              value: 1,
+              gas: 21000,
+              //data: "0x" + toHexString(rlped),
+          }, function (err, hash) {
+              if(err) reject();
+              console.log("> txHash   : ", hash);
+              console.log("send random transaction")
+          });
+          resolve();
+      });
     });
-  });
+  })
+
+  
 
   // increase activeIndex
-  activeIndex++
+  /*activeIndex++
   if (activeIndex > activeAccounts.length){
     activeIndex = 0
-  }
+  }*/
 
 }
 
