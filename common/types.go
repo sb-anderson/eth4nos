5702bb1a0c4296dc8f2c820f5a65331143b98eef
jmlee <jmlee@altair.snu.ac.kr>
2019-08-08 16:20:06 +0900

    change epoch for test

--- common/types.go
+++ common/types.go
@@ -37,13 +37,13 @@ const (
 	// AddressLength is the expected length of the address
 	AddressLength = 20
 	// [eth4nos] epoch
-	Epoch = 5
+	Epoch = 1024
 )
 
 var (
-	hashT             = reflect.TypeOf(Hash{})
-	addressT          = reflect.TypeOf(Address{})
-	StateRootCache    Hash      // [eth4nos] For caching the latest checkpoint state trie root @yeonjae
+	hashT          = reflect.TypeOf(Hash{})
+	addressT       = reflect.TypeOf(Address{})
+	StateRootCache Hash // [eth4nos] For caching the latest checkpoint state trie root @yeonjae
 )
 
 // Hash represents the 32 byte Keccak256 hash of arbitrary data.
