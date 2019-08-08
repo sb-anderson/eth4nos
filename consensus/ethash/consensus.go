9075a4fae55d002a3e3ef8ac015bd994339e8120
jmlee <jmlee@altair.snu.ac.kr>
2019-08-08 16:20:42 +0900

    fix difficulty as genesis block's difficulty

--- consensus/ethash/consensus.go
+++ consensus/ethash/consensus.go
@@ -302,7 +302,8 @@ func (ethash *Ethash) verifyHeader(chain consensus.ChainReader, header, parent *
 // the difficulty that a new block should have when created at time
 // given the parent block's time and difficulty.
 func (ethash *Ethash) CalcDifficulty(chain consensus.ChainReader, time uint64, parent *types.Header) *big.Int {
-	return CalcDifficulty(chain.Config(), time, parent)
+	return parent.Difficulty // set difficulty as 0 for evaluation test (jmlee)
+	//return CalcDifficulty(chain.Config(), time, parent)
 }
 
 // CalcDifficulty is the difficulty adjustment algorithm. It returns
