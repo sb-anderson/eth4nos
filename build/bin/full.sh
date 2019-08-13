ccd343e74bbd80b2a3d929caf5fd918f36778b3a
ricegood <yeonjaekoo@gmail.com>
2019-08-13 20:51:06 +0900

    Configuration files

--- build/bin/full.sh
+++ build/bin/full.sh
@@ -1,12 +1,1 @@
-if [ $1 -eq 1 ]
-then
-  ./geth --datadir "./db/db_full" --keystore "./keystore" --gcmode archive --networkid 12345 --rpc --rpcport "8081" --rpccorsdomain "*" --port 30303 --nodiscover --rpcapi="admin,db,eth,debug,miner,net,shh,txpool,personal,web3" --allow-insecure-unlock --preload "./scripts/script.js" console
-elif [ $1 -eq 2 ]
-then
-  ./geth --datadir "./db/db_full" --keystore "./keystore" --gcmode archive --networkid 12345 --rpc --rpcport "8081" --rpccorsdomain "*" --port 30303 --nodiscover --rpcapi="admin,db,eth,debug,miner,net,shh,txpool,personal,web3" --allow-insecure-unlock --preload "./experiment/expScriptInner.js" console
-elif [ $1 -eq 3 ]
-then
-  ./geth --datadir "./db/db_full_noarchive" --keystore "./keystore" --networkid 12347 --rpc --rpcport "8083" --rpccorsdomain "*" --port 30305 --nodiscover --rpcapi="admin,db,eth,debug,miner,net,shh,txpool,personal,web3" --allow-insecure-unlock --preload "./experiment/expScriptInner.js" console
-else
-    ./geth --datadir "./db/db_full" --keystore "./keystore" --gcmode archive --networkid 12345 --rpc --rpcport "8081" --rpccorsdomain "*" --port 30303 --nodiscover --rpcapi="admin,db,eth,debug,miner,net,shh,txpool,personal,web3" --allow-insecure-unlock console
-fi
+./geth --datadir "./db/db_full" --keystore "./keystore" --gcmode archive --networkid 12345 --rpc --rpcport "8081" --rpccorsdomain "*" --port 30303 --nodiscover --rpcapi="admin,db,eth,debug,miner,net,shh,txpool,personal,web3" --allow-insecure-unlock console
--- build/bin/init.sh
+++ build/bin/init.sh
@@ -1,4 +1,2 @@
 rm -rf db/db_full
-rm -rf db/db_sync
 ./geth --datadir "./db/db_full" init genesis.json
-./geth --datadir "./db/db_sync" init genesis.json
