f377c58f972958053a1689afbb06852e2034a5db
ricegood <yeonjaekoo@gmail.com>
2019-08-14 14:07:24 +0900

    Automatically fast sync & log

--- build/bin/experiment/evaluation.py
+++ build/bin/experiment/evaluation.py
@@ -1,5 +1,5 @@
 from web3 import Web3
-import os
+import socket,os
 
 # Log period
 SIZE_CHECK_PERIOD   = 5
@@ -15,6 +15,7 @@ SYNC_SCRIPT_PATH    = "./fastsync.js"
 # Settings
 FULL_PORT           = "8081"
 SYNC_PORT           = "8082"
+READY_PORT          = "8083"
 PASSWORD            = "1234"
 
 # providers
@@ -31,9 +32,9 @@ def main():
     # get starting block number
     startBlock = fullnode.eth.blockNumber
     currentBlock = fullnode.eth.blockNumber
+    print("Start from block#", startBlock)
     # main loop for send txs
     for i, n in enumerate(numOfTx[startBlock:]):
-        print("i : ", i)
         # send txs for next block
         print("NEXT BLOCK NUM #", currentBlock+1, ", TX #", n)
         for j in range(n):
@@ -54,25 +55,35 @@ def main():
             fastSync(currentBlock)
 
 def sendTransaction():
-    # FIXME: data becomes from address???
     fullnode.eth.sendTransaction({'to': '0xd3CdA913deB6f67967B99D67aCDFa1712C293601', 'from': fullnode.eth.coinbase, 'value': '1', 'data': fullnode.eth.coinbase, 'gas': '210000'})
 
 def sizeCheck(n):
+    # (LOG: block# db_size)
     Cmd = "printf \"" + str(n) + " \" >> " + DB_LOG_PATH
     os.system(Cmd)
     Cmd = "du -sc " + DB_PATH + "geth/chaindata | cut -f1 | head -n 1 >> " + DB_LOG_PATH
     os.system(Cmd)
 
 def fastSync(n):
-    # remove old db
-    Cmd = "rm -rf " + SYNC_DB_PATH
+    # connecting to the fast sync server 
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    s.connect(("localhost", int(READY_PORT)))
+    # check syncnode provider connection
+    syncdone = None
+    while syncdone is None:
+        try:
+            syncdone = syncnode.eth.syncing
+        except:
+            pass
+    while syncdone is False:
+        syncdone = syncnode.eth.syncing
+    print("SYNC DONE!")
+    # after sync done (LOG: block# total_db_size)
+    Cmd = "printf \"" + str(n) + " \" >> " + SYNC_LOG_PATH
     os.system(Cmd)
-    # init fast sync node
-    Cmd = "../geth --datadir \"" + SYNC_DB_PATH + "\" init ../genesis.json"
+    Cmd = "du -sc " + SYNC_DB_PATH + "geth/chaindata | cut -f1 | head -n 1 >> " + SYNC_LOG_PATH
     os.system(Cmd)
-    # run fast sync node
-    Cmd = "../geth --datadir \"" + SYNC_DB_PATH + "\" --syncmode \"fast\" --networkid 12345 --rpc --rpcport \"" + SYNC_PORT + "\" --rpccorsdomain \"*\" --port 30304 --nodiscover --rpcapi=\"admin,db,eth,debug,miner,net,shh,txpool,personal,web3\" --ipcdisable --preload \"" + SYNC_SCRIPT_PATH + "\" > " + SYNC_LOG_PATH + str(n)
-    os.system(Cmd)
+    # TODO:terminate fast sync node
 
 if __name__ == "__main__":
     main()
