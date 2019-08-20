ae3331993c3ef19af661ff42962a214c4dcf8558
ricegood <yeonjaekoo@gmail.com>
2019-08-20 15:09:20 +0900

    Init

--- build/bin/init.sh
+++ build/bin/init.sh
@@ -1,18 +1,3 @@
-fullDataDir="/data/db_full"
-syncDataDir="/data/db_sync"
-
+fullDataDir="./db/db_full"
 rm -rf ${fullDataDir}
-rm -rf ${syncDataDir}
-
-if [[ $1 -eq 1 ]]
-then
-	./geth --datadir ${fullDataDir} init ./genesis/genesis_7000001_7000030.json
-	./geth --datadir ${syncDataDir} init ./genesis/genesis_7000001_7000030.json
-elif [[ $1 -eq 2 ]]
-then
-	./geth --datadir ${fullDataDir} init ./genesis/genesis_7000000.json
-	./geth --datadir ${syncDataDir} init ./genesis/genesis_7000000.json
-else
-	./geth --datadir ${fullDataDir} init genesis.json
-	./geth --datadir ${syncDataDir} init genesis.json
-fi
+./geth --datadir ${fullDataDir} init genesis.json
