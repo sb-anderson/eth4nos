c4cc3d18ce690be5df6f62e4dcbe73b0e47a316d
ricegood <yeonjaekoo@gmail.com>
2019-08-23 16:46:09 +0900

    Update path and scripts

--- build/bin/experiment/simulation.py
+++ build/bin/experiment/simulation.py
@@ -1,27 +1,32 @@
 from web3 import Web3
-import sys,socket,os,random,time
+import sys
+import socket
+import os
+import random
 import mongoAPI
+import json
+import rlp
+import time
 
 # Log period
-SIZE_CHECK_PERIOD   = 5
-EPOCH               = 1024
-#FAST_SYNC_PERIOD    = 1024
+SIZE_CHECK_PERIOD = 100
+EPOCH = 1024
 
 # Path
-DB_PATH             = "../db/db_full_geth/"
-SYNC_DB_PATH        = "../db/db_sync_geth/"
-DB_LOG_PATH         = "./sizelog_geth"
-SYNC_LOG_PATH       = "./synclog_geth"
+DB_PATH = "/data/db_full_get/"
+SYNC_DB_PATH = "/data/db_sync_geth/"
+DB_LOG_PATH = "./sizelog_geth"
+SYNC_LOG_PATH = "./synclog_geth"
 
 # Settings
-FULL_PORT           = "8084"
-SYNC_PORT           = "8085"
-READY_PORT          = "8086"
-PASSWORD            = "1234"
+FULL_PORT = "8084"
+SYNC_PORT = "8085"
+READY_PORT = "8086"
+PASSWORD = "1234"
 
 # Block numbers
-START_BLOCK_NUM     = int(sys.argv[1])
-END_BLOCK_NUM       = int(sys.argv[2])
+START_BLOCK_NUM = int(sys.argv[1])
+END_BLOCK_NUM = int(sys.argv[2])
 
 # providers
 fullnode = Web3(Web3.HTTPProvider("http://localhost:" + FULL_PORT))
@@ -31,32 +36,41 @@ enode = fullnode.geth.admin.nodeInfo()['enode']
 # functions
 def main():
     print("From block#", START_BLOCK_NUM, " to #", END_BLOCK_NUM)
+
     # unlock coinbase
     fullnode.geth.personal.unlockAccount(fullnode.eth.coinbase, PASSWORD, 0)
+
     # get current block
     startBlock = fullnode.eth.blockNumber
     currentBlock = fullnode.eth.blockNumber
+
     # main loop for send txs
     for i in range(START_BLOCK_NUM, END_BLOCK_NUM+1):
-        transactions = mongoAPI.findMany('transactions_test', i)
+	transactions = mongoAPI.findMany('transactions_15', ['blockNum'], [i])
         txNumber = len(transactions)
-        # send txs for next block
-        print("CURRENT BLOCK #", currentBlock, "NEXT DB_BLOCK #", i, ", TX #", txNumber)
+        
+	# send txs for next block
+        print("CURRENT BLOCK #%07d" % currentBlock, end=', ')
+        print("NEXT DB_BLOCK #%07d" % i, end=', ')
+        print("TX #%05d" % txNumber, end=', ')
+        print("RESTORE #%05d" % len(mapper[currentBlock]))
+
         for j in range(txNumber):
             to = transactions[j]['to']
             delegatedFrom = transactions[j]['from']
             sendTransaction(to, delegatedFrom)
-            print("\tSend Tx#", j)
-        # start mining
-        fullnode.geth.miner.start(1)
-        # wait for mining
+	    print("Send Tx# {0}".format(j), end="\r")
+
+        # mining
+        fullnode.geth.miner.start(1) # start mining
         while (currentBlock <= startBlock+i-START_BLOCK_NUM):
-            currentBlock = fullnode.eth.blockNumber
-        # stop mining
-        fullnode.geth.miner.stop()
+            currentBlock = fullnode.eth.blockNumber # wait for mining
+        fullnode.geth.miner.stop() # stop mining
+
         # size check
         if currentBlock % SIZE_CHECK_PERIOD == 0:
             sizeCheck(currentBlock)
+
         # fast sync
         pivotBlockMod = abs(currentBlock-64) % EPOCH
         if pivotBlockMod == EPOCH-1:
@@ -68,8 +82,11 @@ def main():
                     break
                 synced = fastSync(currentBlock)
 
+
 def sendTransaction(to, delegatedFrom):
-    fullnode.eth.sendTransaction({'to': to, 'from': fullnode.eth.coinbase, 'value': '0', 'data': delegatedFrom, 'gas': '0'})
+    fullnode.eth.sendTransaction(
+        {'to': to, 'from': fullnode.eth.coinbase, 'value': '0', 'data': delegatedFrom, 'gas': '0'})
+
 
 def sizeCheck(n):
     # (LOG: block# db_size)
@@ -77,6 +94,7 @@ def sizeCheck(n):
     os.system(Cmd)
     Cmd = "du -sc " + DB_PATH + "geth/chaindata | cut -f1 | head -n 1 >> " + DB_LOG_PATH
     os.system(Cmd)
+
 
 def fastSync(n):
     print("FAST SYNC START!")
