b283d5fb2c10e1839bf00f8bf6d076fbe424ce29
ricegood <yeonjaekoo@gmail.com>
2019-08-23 16:22:15 +0900

    Update simulation with tmp.py

--- build/bin/experiment/simulation.py
+++ build/bin/experiment/simulation.py
@@ -3,8 +3,10 @@ import sys
 import socket
 import os
 import random
-import time
 import mongoAPI
+import json
+import rlp
+import time
 
 # Log period
 SIZE_CHECK_PERIOD = 5
@@ -33,35 +35,50 @@ enode = fullnode.geth.admin.nodeInfo()['enode']
 
 # functions
 def main():
+    f = open("./mapper.json", 'r')
+    mapper = f.read()
+    mapper = json.loads(mapper)
+    f.close()
+
     print("From block#", START_BLOCK_NUM, " to #", END_BLOCK_NUM)
+
     # unlock coinbase
     fullnode.geth.personal.unlockAccount(fullnode.eth.coinbase, PASSWORD, 0)
+
     # get current block
     startBlock = fullnode.eth.blockNumber
     currentBlock = fullnode.eth.blockNumber
+
     # main loop for send txs
     for i in range(START_BLOCK_NUM, END_BLOCK_NUM+1):
-        transactions = mongoAPI.findMany(
-            'transactions_test', ['blockNum'], [i])
+	transactions = mongoAPI.findMany('transactions_15', ['blockNum'], [i])
         txNumber = len(transactions)
-        # send txs for next block
-        print("CURRENT BLOCK #", currentBlock,
-              "NEXT DB_BLOCK #", i, ", TX #", txNumber)
+        
+	# send txs for next block
+        print("CURRENT BLOCK #%07d" % currentBlock, end=', ')
+        print("NEXT DB_BLOCK #%07d" % i, end=', ')
+        print("TX #%05d" % txNumber, end=', ')
+        print("RESTORE #%05d" % len(mapper[currentBlock]))
+
         for j in range(txNumber):
             to = transactions[j]['to']
             delegatedFrom = transactions[j]['from']
             sendTransaction(to, delegatedFrom)
-            print("\tSend Tx#", j)
-        # start mining
-        fullnode.geth.miner.start(1)
-        # wait for mining
+	    print("Send Tx# {0}".format(j), end="\r")
+
+        # restore transaction
+        sendRestoreTx(currentBlock, mapper[currentBlock])
+
+        # mining
+        fullnode.geth.miner.start(1) # start mining
         while (currentBlock <= startBlock+i-START_BLOCK_NUM):
-            currentBlock = fullnode.eth.blockNumber
-        # stop mining
-        fullnode.geth.miner.stop()
+            currentBlock = fullnode.eth.blockNumber # wait for mining
+        fullnode.geth.miner.stop() # stop mining
+
         # size check
         if currentBlock % SIZE_CHECK_PERIOD == 0:
             sizeCheck(currentBlock)
+
         # fast sync
         pivotBlockMod = abs(currentBlock-64) % EPOCH
         if pivotBlockMod == EPOCH-1:
@@ -126,6 +143,67 @@ def fastSync(n):
         return True
     except:
         return False
+
+def sendRestoreTx(currentBlock, addresses):
+    latestCheckPoint = currentBlock - (currentBlock % EPOCH) - 1
+    latestCheckPoint = 0 if latestCheckPoint < 0 else latestCheckPoint
+
+    for r, address in enumerate(addresses):
+        proofs = list()
+        targetBlocks = list(range(latestCheckPoint - EPOCH, 0, -EPOCH))
+        for targetBlock in targetBlocks:
+            proof = fullnode.eth.getProof(
+                Web3.toChecksumAddress(address),
+                [],
+                block_identifier=targetBlock
+            )
+            proofs.append(proof)
+            if proof['restored']:
+                break
+
+        print(currentBlock, proofs, targetBlocks)
+
+        proofs.reverse()
+        targetBlocks.reverse()
+
+        """
+        Compact Form Proof
+        """
+        tmps = proofs[:]  # deep copy
+        for tmp in tmps:
+            if tmp['IsVoid']:
+                proofs.pop(0)
+                targetBlocks.pop(0)
+
+        tmps = proofs[:]
+        for i, tmp in enumerate(tmps):
+            try:
+                if (tmps[i + 1])['IsVoid']:
+                    break
+                else:
+                    proofs.pop(0)
+                    targetBlocks.pop(0)
+            except:
+                pass
+
+        preRlp = list()
+        preRlp.append(address)
+        preRlp.append(0 if len(targetBlocks) == 0 else targetBlocks[0])
+        for proof in proofs:
+            preRlp.append(1 if proof['isBloom'] else 0)
+            if not proof['isBloom']:
+                pfs = proof['accountProof']
+                preRlp.append(len(pfs))
+                for pf in pfs:
+                    preRlp.append(pf)
+
+        # print("> preRlp: ", preRlp)
+
+        rlped = rlp.encode(preRlp)
+        # print("> rlped : ", rlped)
+
+        sendTransaction("0x0123456789012345678901234567890123456789", rlped)
+        print("Restore Tx# {0}".format(r), end="\r")
 
 if __name__ == "__main__":
     main()
