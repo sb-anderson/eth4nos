5ef8239941b9511ee9dae686604c31f417192ae3
jmlee <jmlee@altair.snu.ac.kr>
2019-08-28 17:19:17 +0900

    test bloom filter

--- tests/every_test.go
+++ tests/every_test.go
@@ -2,18 +2,32 @@ package tests
 
 import (
 	"fmt"
+	"math/big"
 
-	"github.com/eth4nos/go-ethereum/common"
+	"github.com/eth4nos/go-ethereum/core/types"
 )
 
 func Example1() {
 
-	b := []byte("0xc4422d1c18e9ead8a9bb98eb0d8bb9dbdf281777")
-	fmt.Println("b:", b)
-	s := string(b)
-	fmt.Println(s)
-	isValidAddress := common.IsHexAddress(s)
-	fmt.Println(isValidAddress)
+	positive := []string{
+		"testtest",
+		"test",
+		"hallo",
+	}
+
+	var bloom types.Bloom
+	for _, data := range positive {
+		bloom.Add(new(big.Int).SetBytes([]byte(data)))
+	}
+
+	fmt.Println("bloom:", bloom)
+	fmt.Println("bloom hash:", bloom.Hash().Hex())
+	bloom.SetBytes([]byte{1})
+	fmt.Println("bloom:", bloom)
+
+	var newBloom types.Bloom
+	newBloom.SetBytes([]byte{1})
+	fmt.Println("new bloom", newBloom)
 
 	// output: 1
 }
