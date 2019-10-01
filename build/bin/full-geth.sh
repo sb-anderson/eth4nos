0f031ecfaf9ade7d24a590e912c5f8429f23a496
ricegood <yeonjaekoo@gmail.com>
2019-10-01 18:24:42 +0900

    Scripts for full node

---
+++ build/bin/full-geth.sh
@@ -0,0 +1,1 @@
+./geth --datadir "./data/geth_1000000_full" --keystore "./keystore" --gcmode archive --networkid 12345 --rpc --rpcport "8081" --rpccorsdomain "*" --port 30303 --nodiscover --rpcapi="admin,db,eth,debug,miner,net,shh,txpool,personal,web3" --allow-insecure-unlock console
