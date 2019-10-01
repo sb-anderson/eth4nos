e8b0438e098e8f754e6e11e9e6dd6b04785c89c9
ricegood <yeonjaekoo@gmail.com>
2019-10-01 18:11:59 +0900

    Scripts for sync experiment

---
+++ build/bin/sync-experiment/fastsync.py
@@ -0,0 +1,44 @@
+import socket,os
+
+# Port
+SYNC_PORT           = "8085"
+READY_PORT          = "8086"
+
+# Path
+GENESIS_PATH   = "../genesis.json"
+DB_PATH        = "../data/"
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
+sock.bind(("localhost", int(READY_PORT)))  
+sock.listen(5)
+dir_name = ""
+file_name = ""
+
+while True:  
+    print("WAIT FAST SYNC....")
+    connection, address = sock.accept()
+    while True:
+        print("wait")
+        data = connection.recv(1024)
+        if not data:
+            print("no data")
+            continue
+        # decode to unicode string 
+        strings = str(data, 'utf8')
+        dir_name, file_name = strings.split(",")
+        print("done")
+        break
+    print("START FAST SYNC! [" , file_name, "]")
+
+    # remove old db
+    Cmd = "rm -rf " + DB_PATH + dir_name
+    os.system(Cmd)
+    
+    # init fast sync node
+    Cmd = "../geth --datadir=\"" + DB_PATH + dir_name + "\" init " + GENESIS_PATH
+    os.system(Cmd)
+
+    # run fast sync node
+    Cmd = "../geth --datadir \"" + DB_PATH + dir_name + "\" --syncmode \"fast\" --networkid 12346 --rpc --rpcport \"" + SYNC_PORT + "\" --rpccorsdomain \"*\" --port 30306 --nodiscover --rpcapi=\"admin,db,eth,debug,miner,net,shh,txpool,personal,web3\" --ipcdisable console 2>&1 | tee " + dir_name + "/" + file_name + ".txt"
+    os.system(Cmd)
+    print("FAST SYNC DONE!")
---
+++ build/bin/sync-experiment/simulation.py
@@ -0,0 +1,65 @@
+from web3 import Web3
+import sys
+import socket
+import os
+import time
+
+# Settings
+FULL_PORT = "8084"
+SYNC_PORT = "8085"
+READY_PORT = "8086"
+
+# Sync settings for directory names
+SYNC_CLIENT = sys.argv[1] # 1:Geth(Original) 2:Geth(No tx,receipts) 3:Eth4nos
+SYNC_BOUNDARY = sys.argv[2]
+SYNC_NUMBER = int(sys.argv[3])
+
+# Paths
+DIR_NAME = "log-" + SYNC_CLIENT + "-" + SYNC_BOUNDARY + "-" + str(SYNC_NUMBER)
+
+# Providers
+fullnode = Web3(Web3.HTTPProvider("http://localhost:" + FULL_PORT))
+syncnode = Web3(Web3.HTTPProvider("http://localhost:" + SYNC_PORT))
+enode = fullnode.geth.admin.nodeInfo()['enode']
+
+# Functions
+def main():
+    # Create log directory
+    print("Make directory [", DIR_NAME, "]")
+    Cmd = "mkdir " + DIR_NAME
+    os.system(Cmd)
+
+    # Fast sync for SYNC_NUMBER times
+    for i in range(SYNC_NUMBER):
+        start_sync = time.time()
+        synced = fastSync(i)
+        while not synced:
+            # try to keep fast sync while 3 hour
+            if time.time() - start_sync >= 10800:
+                break
+            synced = fastSync(i)
+
+def fastSync(n):
+    print("Start "+ str(n) + "th fast sync")
+    file_name = "log-" + SYNC_CLIENT + "-" + SYNC_BOUNDARY + "-" + str(n)
+    try:
+        # connecting to the fast sync server 
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+        s.connect(("localhost", int(READY_PORT)))
+        s.send(bytes(DIR_NAME + "," + file_name, 'utf8'))
+        # check syncnode provider connection
+        connected = syncnode.isConnected()
+        while not connected:
+            connected = syncnode.isConnected()
+        # start sync
+        syncnode.geth.admin.addPeer(enode)
+        # wait until whole fast sync done and terminate
+        while connected:
+            connected = syncnode.isConnected()
+        return True
+    except:
+        return False
+
+if __name__ == "__main__":
+    main()
+    print("DONE")
