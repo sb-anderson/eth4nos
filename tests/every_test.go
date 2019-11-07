a315510c88eae2d767bec194e9a47ab3662429cd
jmlee <jmlee@altair.snu.ac.kr>
2019-11-07 11:19:14 +0900

    state bloom test code

--- tests/every_test.go
+++ tests/every_test.go
@@ -1,33 +1,51 @@
 package tests
 
 import (
-	"fmt"
 	"math/big"
+	"strconv"
 
 	"github.com/eth4nos/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/common"
 )
 
 func Example1() {
 
-	positive := []string{
-		"testtest",
-		"test",
-		"hallo",
+	// make empty state bloom
+	stateBloom := types.StateBloom{0}
+
+	// account num to insert into bloom
+	accNum := 500
+
+	for i := 0; i < accNum; i++ {
+
+		// make address
+		addr := common.HexToAddress(strconv.Itoa(i)) // int to address
+		//addr := common.HexToAddress("0x279a949702698ac715da980aD22951315e7cF490")	// hex to address
+
+		// insert into bloom
+		stateBloom.Add(new(big.Int).SetBytes(addr[:]))
+
 	}
 
-	var bloom types.Bloom
-	for _, data := range positive {
-		bloom.Add(new(big.Int).SetBytes([]byte(data)))
+	for i := accNum; i < 2*accNum; i++ {
+
+		addr := common.HexToAddress(strconv.Itoa(i))
+
+		// check if there is address or not (it can be false positive answer)
+		isExist := stateBloom.TestBytes(addr[:])
+
+		if isExist {
+			println("i: ", i, " / address: ", addr.Hex(), "isExist: ", isExist)
+		}
 	}
 
-	fmt.Println("bloom:", bloom)
-	fmt.Println("bloom hash:", bloom.Hash().Hex())
-	bloom.SetBytes([]byte{1})
-	fmt.Println("bloom:", bloom)
-
-	var newBloom types.Bloom
-	newBloom.SetBytes([]byte{1})
-	fmt.Println("new bloom", newBloom)
+	// blockNum := big.NewInt(345599)
+	// blockHash := rawdb.ReadCanonicalHash(rawdb.GlobalDB, blockNum.Uint64())
+	// blockHeader := rawdb.ReadHeader(rawdb.GlobalDB, blockHash, blockNum.Uint64())
+	// stateBloomBytes, _ := rawdb.ReadBloomFilter(rawdb.GlobalDB, blockHeader.StateBloomHash)
+	// stateBloom := types.BytesToStateBloom(stateBloomBytes)
+	// addr := common.HexToAddress("0x279a949702698ac715da980aD22951315e7cF490")
+	// stateBloom.Add(new(big.Int).SetBytes(addr[:]))
 
 	// output: 1
 }
