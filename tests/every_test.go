d394280d756491bc16c255f11fe0d7c71e1289d0
jmlee <jmlee@altair.snu.ac.kr>
2019-11-07 11:41:49 +0900

    add random address generator

--- tests/every_test.go
+++ tests/every_test.go
@@ -1,6 +1,8 @@
 package tests
 
 import (
+	"crypto/rand"
+	"encoding/hex"
 	"math/big"
 	"strconv"
 
@@ -8,19 +10,33 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 )
 
+func randomHex(n int) string {
+	bytes := make([]byte, n)
+	if _, err := rand.Read(bytes); err != nil {
+		return ""
+	}
+	return hex.EncodeToString(bytes)
+}
+
 func Example1() {
 
 	// make empty state bloom
 	stateBloom := types.StateBloom{0}
 
 	// account num to insert into bloom
-	accNum := 500
+	accNum := 1
 
 	for i := 0; i < accNum; i++ {
 
-		// make address
-		addr := common.HexToAddress(strconv.Itoa(i)) // int to address
-		//addr := common.HexToAddress("0x279a949702698ac715da980aD22951315e7cF490")	// hex to address
+		// make address (int to address)
+		addr := common.HexToAddress(strconv.Itoa(i))
+
+		// make address (hex to address)
+		//addr := common.HexToAddress("0x279a949702698ac715da980aD22951315e7cF490")
+
+		// make address (random)
+		// randHex := randomHex(20)
+		// randAddr := common.HexToAddress(randHex)
 
 		// insert into bloom
 		stateBloom.Add(new(big.Int).SetBytes(addr[:]))
@@ -38,14 +54,6 @@ func Example1() {
 			println("i: ", i, " / address: ", addr.Hex(), "isExist: ", isExist)
 		}
 	}
-
-	// blockNum := big.NewInt(345599)
-	// blockHash := rawdb.ReadCanonicalHash(rawdb.GlobalDB, blockNum.Uint64())
-	// blockHeader := rawdb.ReadHeader(rawdb.GlobalDB, blockHash, blockNum.Uint64())
-	// stateBloomBytes, _ := rawdb.ReadBloomFilter(rawdb.GlobalDB, blockHeader.StateBloomHash)
-	// stateBloom := types.BytesToStateBloom(stateBloomBytes)
-	// addr := common.HexToAddress("0x279a949702698ac715da980aD22951315e7cF490")
-	// stateBloom.Add(new(big.Int).SetBytes(addr[:]))
 
 	// output: 1
 }
