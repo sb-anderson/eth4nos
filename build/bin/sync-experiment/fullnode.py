3f981472c9048f4b7dff2529e49c4f30a812906b
ricegood <951227@gmail.com>
2020-09-29 17:27:13 +0900

    Scripts for fullnode

--- build/bin/sync-experiment/fullnode.py
+++ build/bin/sync-experiment/fullnode.py
@@ -3,33 +3,37 @@ import socket,os
 # Port
 FULL_PORT           = "8081"
 FULL_READY_PORT     = "8084"
+FULL_ADDR           = "147.46.115.21"
 
 # Path
 GENESIS_PATH   = "../genesis.json"
-DB_PATH        = "/home/jaeykim/data/geth_300000/db_archive"
+DB_PATH        = "/data/geth_archive_30/db_archive"
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
-sock.bind(("localhost", int(FULL_READY_PORT)))  
+sock.bind((FULL_ADDR, int(FULL_READY_PORT)))  
 sock.listen(5)
-dir_name = ""
-file_name = ""
+
+sync_boundaries = ["40383", "80703", "121023", "161343", "201663", "241983", "282303"]
 
 while True:  
     print("WAIT....")
     connection, address = sock.accept()
     while True:
-        print("wait")
+        print("connected!", "(connection:", connection, ", address:", address, ")")
         data = connection.recv(1024)
         if not data:
             print("no data")
-            continue
+            break
+            #continue
         # decode to unicode string 
-        sync_boundary = str(data, 'utf8')
-        print("done")
+        message = str(data, 'utf8')
+        if message in sync_boundaries:
+            sync_boundary = message
+    	    # run full node
+            print("START FULL NODE! [ SyncBoundary : " , sync_boundary, "]")
+            Cmd = "../geth --datadir \"" + DB_PATH + "\" --ethash.dagdir \"" + DB_PATH + "_ethash\" --keystore \"../keystore\" --gcmode archive --networkid 12345 --rpc --rpcaddr \"" + FULL_ADDR + "\" --rpcport \"" + FULL_PORT + "\" --rpccorsdomain \"*\" --port 30303 --rpcapi=\"admin,db,eth,debug,miner,net,shh,txpool,personal,web3\" --allow-insecure-unlock --syncboundary " + sync_boundary + " console"
+            os.system(Cmd)
+            print("FULL NODE DONE!")
+        else:
+            print("wrong message. do not run geth. re-wait. (messages = ", message, ")")
         break
-    print("START FULL NODE! [ SyncBoundary : " , sync_boundary, "]")
-
-    # run full node
-    Cmd = "../geth --datadir \"" + DB_PATH + "\" --ethash.dagdir \"" + DB_PATH + "_ethash\" --keystore \"../keystore\" --gcmode archive --networkid 12345 --rpc --rpcport \"" + FULL_PORT + "\" --rpccorsdomain \"*\" --port 30303 --nodiscover --rpcapi=\"admin,db,eth,debug,miner,net,shh,txpool,personal,web3\" --allow-insecure-unlock --syncboundary " + sync_boundary + " console"
-    os.system(Cmd)
-    print("FULL NODE DONE!")
